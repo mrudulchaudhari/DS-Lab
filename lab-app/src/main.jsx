66248456460c6f6317aa2a6ac133616f1be6d70a
Adhishree21 <adhishreeshiledar21@gmail.com>
2024-09-04 19:27:23 +0530

    updated css in index.css

--- lab-app/src/main.jsx
+++ lab-app/src/main.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './Routes.jsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
