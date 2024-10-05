a1ce620bedcd1095a23523d788a35de1827b2c17
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2024-10-06 01:32:51 +0530

    some random problems

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DS-Lab/',
   plugins: [react()],
-})
+  base: '/DS-Lab/',  // The same as your repo name
+});
